ac8c684535779e5473f0ec059e51bf2ea6b508db
ArijitPatra2906 <patraarijit440@gmail.com>
2025-02-28 16:03:52 +0530

    screener form

--- app/(main)/layout.js
+++
@@ -1,9 +0,0 @@
-import { ProgressLayout } from "@/components/layout/progress-layout";
-
-export default function RootLayout({ children }) {
-  return (
-    <div>
-      <ProgressLayout>{children}</ProgressLayout>
-    </div>
-  );
-}
